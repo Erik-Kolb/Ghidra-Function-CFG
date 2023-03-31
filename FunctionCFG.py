8c164f97ca1628566cafff1af7a5261e25f28670
Erik-Kolb <125336455+Erik-Kolb@users.noreply.github.com>
2023-03-30 23:51:34 -0400

    Update FunctionCFG.py

--- FunctionCFG.py
+++ FunctionCFG.py
@@ -1,5 +1,3 @@
-
-
 from ghidra.program.model.block import SimpleBlockIterator
 from ghidra.program.model.block import BasicBlockModel
 from ghidra.program.model.block import CodeBlockReferenceIterator
@@ -7,76 +5,41 @@ from ghidra.program.model.block import CodeBlockIterator
 from ghidra.program.model.block import CodeBlockReference
 import os.path
 
-#Establish the initial Block
 bbModel = BasicBlockModel(currentProgram)
-for func in currentProgram.getListing().getFunctions(True):  #Iterates through all functions in the program
+for func in currentProgram.getListing().getFunctions(True):
 	f_name = func.getName().strip("LAB_")
 	f_name = f_name.strip("FUN_")
-	f_name = f_name.lstrip('00')
-	path = "C:\\Users\erik_\\OneDrive\\CFG_Files\\" + str(f_name) + ".dot"
+	f_name = f_name.lstrip("00")
+	print(f_name)
+
+	malware = "GreenCat1"
+	dump = "/home/ek/Desktop/Ghidra-Function-CFG/dot_files/malware"
+	if not os.path.exists(dump):
+		os.makedirs(dump)
+
+	path = dump + "/" + str(f_name) + ".dot"
 
 	dot = open(path, "w+")
-        out_f = "digraph " + '"' + f_name + '"' + " {"
-        dot.write(out_f + "\n")
+	out_f = "digraph" + '"' + f_name + '"' + " {"
+	dot.write(out_f + "\n")
 
-	#Creates the Blocks that can be iterated over
 	codeBlockIterator = bbModel.getCodeBlocksContaining(func.getBody(), monitor)
 	bb = codeBlockIterator.next()
-	#Iterates and writes to a file all of the destinations of the block
 	while codeBlockIterator.hasNext():
 		successors = bb.getDestinations(monitor)
 		while successors.hasNext():
-			sucBBRef = successors.next()
-			sucBBRefAddr = sucBBRef.getReferent()
 			bb_name = str(bb).split(" ")
 			bb_name = bb_name[0]
 			bb_name = bb_name.strip("LAB_")
 			bb_name = bb_name.strip("FUN_")
-			print('"' + bb_name + '"' + " -> " + '"' + str(sucBBRefAddr) + '"')
-			dot.write('"' + bb_name + '"' + " -> " + '"' + str(sucBBRefAddr) + '"' + ";\n")
+			destination = successors.next()
+			new_block_dest = str(destination).split('> ')[-1]
+			print('"' + bb_name + '"' + "->" + '"' + new_block_dest + '"')
+			dot.write('"' + bb_name + '"' + "->" + '"' + new_block_dest + '"' + ";\n")
 		bb = codeBlockIterator.next()
+
+
 	dot.write("}\n")
 	dot.close()
-	
-	
-	
-	
-	
-	
-#Better Version
-from ghidra.program.model.block import SimpleBlockIterator
-from ghidra.program.model.block import BasicBlockModel
-from ghidra.program.model.block import CodeBlockReferenceIterator
-from ghidra.program.model.block import CodeBlockIterator
-from ghidra.program.model.block import CodeBlockReference
-import os.path
 
-#Establish the initial Block
-bbModel = BasicBlockModel(currentProgram)
-for func in currentProgram.getListing().getFunctions(True):  #Iterates through all functions in the program
-	f_name = func.getName().strip("LAB_")
-	f_name = f_name.strip("FUN_")
-	f_name = f_name.lstrip('00')
-	path = "C:\\Users\erik_\\OneDrive\\CFG_Files\\" + str(f_name) + ".dot"
 
-	dot = open(path, "w+")
-        out_f = "digraph " + '"' + f_name + '"' + " {"
-        dot.write(out_f + "\n")
-
-	#Creates the Blocks that can be iterated over
-	codeBlockIterator = bbModel.getCodeBlocksContaining(func.getBody(), monitor)
-	bb = codeBlockIterator.next()
-	#Iterates and writes to a file all of the destinations of the block
-	while codeBlockIterator.hasNext():
-		successors = bb.getDestinations(monitor)
-		while successors.hasNext():
-			sucBBRef = successors.next()
-			bb_name = str(bb).split(" ")
-			bb_name = bb_name[0]
-			bb_name = bb_name.strip("LAB_")
-			bb_name = bb_name.strip("FUN_")
-			print('"' + bb_name + '"' + " -> " + '"' + str(sucBBRef) + '"')
-			#dot.write('"' + bb_name + '"' + " -> " + '"' + str(sucBBRefAddr) + '"' + ";\n")
-		bb = codeBlockIterator.next()
-	dot.write("}\n")
-	dot.close()
