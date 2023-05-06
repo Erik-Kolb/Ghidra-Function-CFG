686277efe7a85aed97634166b0e2a68f52b82487
Erik-Kolb <125336455+Erik-Kolb@users.noreply.github.com>
2023-05-05 20:46:00 -0400

    Update FunctionCFG.py

--- FunctionCFG.py
+++ FunctionCFG.py
@@ -4,32 +4,26 @@ import networkx as nx
 from networkx.drawing.nx_agraph import write_dot
 import argparse
 from tqdm import tqdm
-import logging
+# import logging
 
-logging.basicConfig()
-logging.getLogger().setLevel(logging.DEBUG)
+# logging.basicConfig()
+# logging.getLogger().setLevel(#logging.DEBUG)
 
 # use argpase to take command line inputs
 # command line inputs are the directory that contains dot files for each
 # function
 
-base = "/home/ek/Desktop/Ghidra-Function-CFG/results/"
-res = os.path.join(base, 'greencat1')
 
-with open(base + "/greencat1/imports.json", 'r') as of:
-    imports = json.load(of)
-
-imports_list = [v for k,v in imports.items()]
-
-def write_rebuilt_dot(G, d):
+def write_rebuilt_dot(G, d, file):
     name = d.split('/')[-1].split('.dot')[0]
-    directory = res + '/rebuilt_dot/'
+    directory = file + '/rebuilt_dot/'
     if not os.path.exists(directory):
         os.makedirs(directory)
     path = directory + name + '.dot'
     write_dot(G, path)
 
-def dot_to_nx_graph(dot_file):
+
+def dot_to_nx_graph(dot_file, file):
 
     with open(dot_file, 'r') as dot:
         data = dot.read().splitlines()
@@ -56,19 +50,18 @@ def dot_to_nx_graph(dot_file):
                 else:
                     G.add_node(e)
 
-                G.add_node(s)                
+                G.add_node(s)
                 G.add_edge(s, e)
 
             elif '=' in line:
                 entry_node = line.split('entry: ')[-1].split(',')[0]
                 exit_node = line.split('exit: ')[-1].rstrip('"')
-        
 
         # add edges from API calls back into the function
         adjacency_matrix = {}
         for a in G.adjacency():
             adjacency_matrix[a[0]] = a[1]
-     
+
         for edge in G.edges():
             s = edge[0]
             e = edge[1]
@@ -78,21 +71,20 @@ def dot_to_nx_graph(dot_file):
                 for val in vals:
                     G.add_edge(e, val)
 
-    write_rebuilt_dot(G, dot_file)
+    write_rebuilt_dot(G, dot_file, file)
     return G, entry_node, exit_node
 
 
 # invoke networkx to identify paths from an entry point to exit point
 def generate_function_paths(G, entry, ext):
 
-    logging.info("Generating all paths from {} to {}".format(entry, ext))
     paths = []
     for path in nx.all_simple_paths(G, entry, ext):
         paths.append(path)
     return paths
 
-def imports_only(paths):
-    logging.info("Stripping non-imports from paths")
+
+def imports_only(paths, imports_list):
     npaths = []
     for p in paths:
         # dump all basic blocks that are not calls to imports
@@ -106,56 +98,108 @@ def imports_only(paths):
     return sorted(npaths, key=len)
 
 # print lists with carriage return
+
+
 def ppp(paths):
     i = 1
     for p in paths:
-        logging.info("{}: {}".format(i, p))
+        # logging.info("{}: {}".format(i, p))
         i += 1
-#This is the dictionary that takes a line number as input and gives the capability based on the paths of the fuction
-capability_sequences = {1: "Get Drive Info", 2: "Start Shell", 3:"Start Service", 4:"whoami", 5:"Create Copy of Running Process", 6: "Create Copy of Running Process", 7:"File by URL", 8:"Current Directory", 9:"Chagne Directory", 10:"Connect to C&C", 11: "Disconnect from C&C", 12:"Sart Hidden Service", 13:"Push/Pull from C&C", 14:"Drive Info"}
+# This is the dictionary that takes a line number as input and gives the
+# capability based on the paths of the fuction
 
-#Checks sequences of a function for capability
-def capabilities(api_sequence, model_file, capability_dict):
-    count = 0
-    with open(model_file) as models:
+# Checks sequences of a function for capability
+
+def extract_API_sequence(api_sequence, capability_models):
+
+    with open(capability_models) as models:
         for model in models:
-            count += 1
             model = model.strip("\n")
-            model = model.split(", ")
-            model_len = len(model)
+            model = model.split(" - ")
+            capability = model[1]
+            model = model[0]
+            model_list = model.split(", ")
+            model_len = len(model_list)
+
             sequence_len = len(api_sequence)
-            if model_len > sequence_len:
-                for i in range(model_len - sequence_len +1):
-                    if model[i:i+sequence_len] == api_sequence:
-                        capability = capability_dict[count]
-                        print(capability)
+            if model_len <=  sequence_len:
+                for i in range(model_len - sequence_len + 1):
+                    if model_list[i:i + sequence_len] == api_sequence:
+                        return capability, str(api_sequence)
 
-def main():
 
-    files = [os.path.join(res, x) for x in os.listdir(res) if '.dot' in x]
-    
+def capability_analysis(results, imports_list, capability_models):
+
+    files = [os.path.join(results, x)
+             for x in os.listdir(results) if '.dot' in x]
+
+    malware_name = results.split('/')[-1]
+    print("[+] Analyzing the {} malware.".format(malware_name))
+    capability_path = results.strip(
+        malware_name) + malware_name + "_capabilities.json"
+    capability_results = {malware_name: {}}
     for dot_file in files:
-
-        #if '402645' not in dot_file:
-        #    continue
-        G, en, ex = dot_to_nx_graph(dot_file)
+        G, en, ex = dot_to_nx_graph(dot_file, results)
 
         if not G.nodes:
-            logging.debug("Empty dot file: {}\n".format(dot_file.split('/')[-1].split('.dot')[0]))
+            # logging.debug("Empty dot file: {}\n".format(dot_file.split('/')[-1].split('.dot')[0]))
             continue
-
-        logging.info("Analyzing {}".format(dot_file.split('/')[-1].split('.dot')[0]))
+        function_name = str(dot_file.split('/')[-1].split('.dot')[0])
+        if function_name == "401406" or function_name == "lpStartAddress_00402292" or function_name == "401e12" or function_name == "401ba9" or function_name == "402292":
+            continue
+        # logging.info("Analyzing {}".format(dot_file.split('/')[-1].split('.dot')[0]))
         paths = generate_function_paths(G, en, ex)
-        new_paths = imports_only(paths)
+        new_paths = imports_only(paths, imports_list)
+        capability_list = {}
         for path in new_paths:
-            if type(capabilities(path, "capability_models.txt", capability_sequences)) != type(None):
-                print(capabilities(path, "capability_models.txt", capability_sequences))
+            x = extract_API_sequence(path, capability_models)
+            if x:
+                new_capability = x[0]
+                api_sequence = x[1]
+     
+                if new_capability and str(new_capability) not in capability_list.keys():
+                    capability_list[new_capability] = api_sequence
 
+        if capability_list:
+            
+            capability_results[malware_name][function_name] = capability_list
+            print("[+] Matched capability in function: " + function_name)
+            print(u" \u2558" + " Capability ")
+            for c in capability_list:
+                print(u"   \u257E" + " {}".format(c))
 
-        #ppp(new_paths)
+    with open(capability_path, 'w') as capability_output:
+        json.dump(capability_results, capability_output)
 
-        #import IPython; IPython.embed()
-  
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description="What is the name of the directory?")
+    parser.add_argument(
+        "dir_path",
+        metavar="DIR",
+        type=str,
+        help="The Directory containing the malware to be analyzed")
+    parser.add_argument(
+        "file",
+        metavar="FILE",
+        type=str,
+        help="The File containing the capability models")
+    args = parser.parse_args()
+
+    capabilty_models = args.file
+
+    malware_results_folder = args.dir_path
+    malware_results = [
+        malware_results_folder +
+        x for x in os.listdir(malware_results_folder) if '.json' not in x]
+
+    for mal in malware_results:
+
+        with open(mal + "/imports.json", 'r') as of:
+            imports = json.load(of)
+        imports_list = [v for k, v in imports.items()]
+
+        capability_analysis(mal, imports_list, capabilty_models)
+        print("[+] Analysis complete!\n")
+
